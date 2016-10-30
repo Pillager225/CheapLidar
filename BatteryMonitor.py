bc99c399d30c739d96cf1bd8b244d3fa85a4dd29
Pillager225 <ryanloringcooper@gmail.com>
2016-10-29 22:52:05 -0700

    Stores last 5 readings, averages them, and then makes a decision.

--- BatteryMonitor.py
+++ BatteryMonitor.py
@@ -9,15 +9,33 @@ import sys
 
 class BatteryMonitor:
 	adc = None
+	qSize = 5
+	batVals = [-1]*qSize
+	batValIndex = 0
 
 	def __init__(self):
 		self.adc = ADCCommunicator()
 		self.main()
 
+	def getAverageBatVal(self):
+		ave = 0
+		i = 0
+		for i in range(0, qSize):
+			if self.batVals[i] == -1:
+				break
+			else:
+				ave += self.batVals[i]
+		if i < 3:
+			return -1
+		else:
+			return ave/(i+1)
+
 	def main(self):
 		while(True): 
-			bat = self.adc.askADC(1)
-			if bat <= 195:
+			self.batVals[self.batValIndex] = self.adc.askADC(1)
+			self.batValIndex = (self.batValIndex+1)%self.qSize
+			v = self.getAverageBatVal()
+			if v != -1 and v <= 210:
 				subprocess.call(['sudo', 'shutdown', '-h', 'now'])
 				sys.exit(0)	
 			time.sleep(5)
